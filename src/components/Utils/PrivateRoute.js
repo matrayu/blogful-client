a191e4596a4c248a3524bfdaef0a764c5f47f4af
matrayu <northridge614@gmail.com>
2019-06-19 06:51:32 -0700

    Auth Starting Point Complete
    Work completed after the auth starting point module

--- src/components/Utils/PrivateRoute.js
+++ src/components/Utils/PrivateRoute.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import TokenService from '../../services/token-service'
 
+// export default function PrivateRoute(props) {}
 export default function PrivateRoute({ component, ...props }) {
   const Component = component
   return (
     <Route
       {...props}
-      render={componentProps => (
+      render={rProps => (
         TokenService.hasAuthToken()
-          ? <Component {...componentProps} />
+          ? <Component {...rProps} />
           : <Redirect
               to={{
                 pathname: '/login',
-                state: { from: componentProps.location }
+                state: { from: rProps.location }
               }}
             />
       )}
